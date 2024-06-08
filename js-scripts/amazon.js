c76cc5bcdfb7a7ff3d8818a7b7f681cccd97795d
kevin-arock <kevinstandlee2002@gmail.com>
2024-06-08 22:35:35 +0530

    Use product.js as a module

--- js-scripts/amazon.js
+++ js-scripts/amazon.js
@@ -1,42 +1,5 @@
 import { cart } from "../data/cart.js";
-
-// const products = [
-//     {
-//         img:"images/products/athletic-cotton-socks-6-pairs.jpg",
-//         name:"Black and Gray Athletic Cotton Socks - 6 Pairs",
-//         rating:{
-//                 star:"images/ratings/rating-45.png",
-//                 count:87,
-
-//             },
-//         price:1090
-        
-//     },
-//     {
-//         img:"images/products/intermediate-composite-basketball.jpg",
-//         name:"Intermediate Size Basketball",
-//         rating:{
-//                 star:"images/ratings/rating-40.png",
-//                 count:127,
-
-//             },
-//         price:2095
-        
-//     },
-//     {
-//         img:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-//         name:"Adults Plain Cotton T-Shirt - 2 Pack",
-//         rating:{
-//                 star:"images/ratings/rating-45.png",
-//                 count:56,
-
-//             },
-//         price:799
-        
-//     },
-
-
-// ]
+import { products } from "../data/products.js";
 let productHTML = ``;
 products.forEach((product,i) =>{
      productHTML+= `
